e79a668d8b59bb55bc561b47474c90367728f293
AhsenAydinoglu <ahsenaydinoglu@gmail.com>
2023-01-22 21:33:54 +0300

    Add project files.

---
+++ MiniChatUygulamasi/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniChatUygulamasi.Models;
+
+namespace MiniChatUygulamasi.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly SohbetServisi _sohbetServisi;
+
+        public ChatController(SohbetServisi sohbetServisi)
+        {
+           _sohbetServisi=sohbetServisi;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MesajlarPartial()
+        {
+            return PartialView("_Mesajlar");
+        }
+
+        [HttpPost]
+        public IActionResult Gonder(Mesaj mesaj)
+        {
+            if (ModelState.IsValid)
+            {
+                _sohbetServisi.MesajEkle(mesaj.Gonderen, mesaj.Icerik);
+                return Ok(); //durum kodu 200 (200'lü kodlar başarılı durumları temsil eder.)
+            }
+            return BadRequest(); //durum kodu 400 (400'lü kodlar hatayı temsil eder.)
+        }
+    }
+}
---
+++ MiniChatUygulamasi/Models/Mesaj.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniChatUygulamasi.Models
+{
+    public class Mesaj
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(20)]
+        public string Gonderen { get; set; }
+        [Required, MaxLength(400)]
+        public string Icerik { get; set; }
+        public DateTime Zaman { get; set; } = DateTime.Now;
+    }
+}
